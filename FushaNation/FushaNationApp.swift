bb88632bbd0f5a05fc774c80b6520c3e9e8c853c
Nabeel Khan <nabeel.khan.swe@gmail.com>
2026-03-23 20:45:28 -0700

    Initial Commit

---
+++ FushaNation/FushaNationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FushaNationApp.swift
+//  FushaNation
+//
+//  Created by Nabeel Khan on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct FushaNationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
